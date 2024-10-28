be8601f510ce455701add3355a50f595d7282c46
sofia <sofygonzalez1012@gmail.com>
2024-10-27 20:52:40 -0500

    categories feature

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException
-from models import CustomerCreate
+from models import CustomerCreate, CategoryCreate
 from db_customers_helper import db_customers_get, create_db_customer, create_db_customers_bulk
 from typing import List 
+from db_category_helper import create_db_category, db_get_categories, create_db_categories_bulk
 
 router = APIRouter()
 
@@ -27,4 +28,32 @@ def bulk_customer (customers: List[CustomerCreate]):
         }
     else:
         raise HTTPException(status_code=500, detail="An error occurred while creating customers.")
+
+@router.post("/create/category")
+def create_category (category: CategoryCreate):
+    create_db_category(category)
+
+    return{
+        "message": "Category created successfully."
+    }
+
+@router.get("/get/categories")
+def get_categories ():
+    categories = db_get_categories()
+    return {
+        "categories": categories
+    }
+
+@router.post("/create/categories/bulk")
+def bulk_categories(categories: List[CategoryCreate]):
+    if create_db_categories_bulk (categories):
+        return{
+            "message" : "Categories created successfully"
+        }
+    else:
+        raise HTTPException(status_code=500, detail="An error occurred while creating categories.")
+
+
+
+
 
